bf532b19679f3ab028db63130c2bad28a6a49a3a
nigel <c.fans@foxmail.com>
2017-11-07 10:33:55 +0800

    add currency to Chinese

--- CompanyCommonTask.js
+++ CompanyCommonTask.js
@@ -105,31 +105,85 @@ var ligerHelper = (function () {
 
 // 与财务相关的辅助方法
 var financialHelper = (function () {
-    var chnNumChar = ["零","一","二","三","四","五","六","七","八","九"],
-        chnUnitSection = ["","万","亿","万亿","亿亿"],
-        chnUnitChar = ["","十","百","千"],
-        SectionToChinese;
+    var  convertCurrency;
 
-    SectionToChinese = function (section){
-  var strIns = '', chnStr = '';
-  var unitPos = 0;
-  var zero = true;
-  while(section > 0){
-    var v = section % 10;
-    if(v === 0){
-      if(!zero){
-        zero = true;
-        chnStr = chnNumChar[v] + chnStr;
-      }
-    }else{
-      zero = false;
-      strIns = chnNumChar[v];
-      strIns += chnUnitChar[unitPos];
-      chnStr = strIns + chnStr;
+    convertCurrency = function(money) {
+        var cnNums = new Array('零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'), //汉字的数字
+            cnIntRadice = new Array('', '拾', '佰', '仟'),        //基本单位
+            cnIntUnits = new Array('', '万', '亿', '兆'),         //对应整数部分扩展单位
+            cnDecUnits = new Array('角', '分', '毫', '厘'),       //对应小数部分单位
+            cnInteger = '整',            //整数金额时后面跟的字符
+            cnIntLast = '元',           //整型完以后的单位
+            maxNum = 999999999999999.9999,      //最大处理的数字
+            integerNum,         //金额整数部分
+            decimalNum,         //金额小数部分
+            chineseStr = '',    //输出的中文金额字符串
+            parts;              //分离金额后用的数组，预定义
+
+         if (money == '') { return ''; }
+         money = parseFloat(money);
+         if (money >= maxNum) {
+             //超出最大处理数字
+             return '';
+         }
+         if (money == 0) {
+             chineseStr = cnNums[0] + cnIntLast + cnInteger;
+             return chineseStr;
+         }
+         //转换为字符串
+         money = money.toString();
+         if (money.indexOf('.') == -1) {
+             integerNum = money;
+             decimalNum = '';
+         } else {
+             parts = money.split('.');
+             integerNum = parts[0];
+             decimalNum = parts[1].substr(0, 4);
+         }
+         //获取整型部分转换
+         if (parseInt(integerNum, 10) > 0) {
+             var zeroCount = 0;
+             var IntLen = integerNum.length;
+             for (var i = 0; i < IntLen; i++) {
+                 var n = integerNum.substr(i, 1);
+                 var p = IntLen - i - 1;
+                 var q = p / 4;
+                 var m = p % 4;
+                 if (n == '0') {
+                     zeroCount++;
+                 } else {
+                     if (zeroCount > 0) {
+                         chineseStr += cnNums[0];
+                     }
+                     //归零
+                     zeroCount = 0;
+                     chineseStr += cnNums[parseInt(n)] + cnIntRadice[m];
+                 }
+                 if (m == 0 && zeroCount < 4) {
+                     chineseStr += cnIntUnits[q];
+                 }
+             }
+             chineseStr += cnIntLast;
+         }
+         //小数部分
+         if (decimalNum != '') {
+             var decLen = decimalNum.length;
+             for (var i = 0; i < decLen; i++) {
+                 var n = decimalNum.substr(i, 1);
+                 if (n != '0') {
+                     chineseStr += cnNums[Number(n)] + cnDecUnits[i];
+                 }
+             }
+         }
+         if (chineseStr == '') {
+             chineseStr += cnNums[0] + cnIntLast + cnInteger;
+         } else if (decimalNum == '') {
+             chineseStr += cnInteger;
+         }
+         return chineseStr;
     }
-    unitPos++;
-    section = Math.floor(section / 10);
-  }
-  return chnStr;
-}
+
+    return {
+        convertCurrency: convertCurrency
+    }
 })();
